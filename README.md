2d2acee526ee24a1b2b0b693853c19cb670bf863
RaymondChen <xi.chen2210@gmail.com>
2023-07-31 22:04:09 +1000

    Final adjustments

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # SVG-Logo-Maker
+
+## Description
+This application allows the user to generate simple logos for various purposes.
+It takes inputs for the text you want to be included in the logo (up to 3 characters), then the color of the text.
+Then it also provides selection of shapes: Square, Circle and Triangle, then input of the color of the shape.
+And the application will generate a logo of your choice and export it as an SVG file. 
+## User Story
+AS a freelance web developer
+I WANT to generate a simple logo for my projects
+SO THAT I don't have to pay a graphic designer
+
+## Acceptance Criteria
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+## Walkthrough Video
+[Walkthrough Video](https://watch.screencastify.com/v/ij7IV8C3kivgorsjKHor)
+
+![](./Walkthrough/README_Generator_Walkthrough.gif)
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Insert all required modules from other files and system
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Square, Circle, Triangle} = require('./lib/shape')
+
+// SVG logo content and size of the created logo
 const SVGHeader = `<svg version="1.1"
 width="300" height="200"
 xmlns="http://www.w3.org/2000/svg">`;
 
+const SVGFooter = `</svg>`;
 
+// Contents of the prompt questions, ask for text, text color, shape and shape color
 const questions = [
     {
         type: 'input',
         name: 'logoText',
         message: 'Please enter the text that you want to appear on the SVG logo, up to three letters:',
+        // Error handler if the input has more than 3 characters
         validate: (logoText) => {
             if (logoText.length > 3){
                 console.log('\n Please enter no more than 3 characters')
@@ -38,20 +44,23 @@ const questions = [
     },
 ];
 
+// Confirm selected shape and create a new shape element from the constructor
 function confirmShape(response){
     if (response.shape === 'Square') {
         const confirmedShape = new Square (response.shapeColor, response.logoText, response.textColor);
         console.log(confirmShape);
-        return SVGHeader + confirmedShape.render() + confirmedShape.addText()
+        // Combining contents for SVG logo formatting, selected shape with desired color and selected text with desired color
+        return SVGHeader + confirmedShape.render() + confirmedShape.addText() + SVGFooter
     } else if (response.shape === 'Circle') {
         const confirmedShape = new Circle (response.shapeColor, response.logoText, response.textColor);
-        return SVGHeader + confirmedShape.render() + confirmedShape.addText()
+        return SVGHeader + confirmedShape.render() + confirmedShape.addText() + SVGFooter
     } else if (response.shape === 'Triangle'){
         const confirmedShape = new Triangle (response.shapeColor, response.logoText, response.textColor);
-        return SVGHeader + confirmedShape.render() + confirmedShape.addText()
+        return SVGHeader + confirmedShape.render() + confirmedShape.addText() + SVGFooter
     }
 }
 
+// Start application 
 function init(){
     inquirer.prompt(questions)
         .then ((response) => {
